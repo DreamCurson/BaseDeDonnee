53cabcf7701a88033ecda379f222b28fecd7185a
dreamcurson <dreamcurson@outlook.com>
2025-10-10 07:52:13 -0400

    Finalisation de evenements store avec validation que le post contient les id important + que la session est bien instencié

--- classes/evenements/evenements-store.php
+++ classes/evenements/evenements-store.php
@@ -1,5 +1,28 @@
 <?php
+session_start();
 
-// var_dump($_POST);
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    header('Location: ../../dreamplante.php');
+    exit;
+}
 
-?>
+require_once('../CRUD.php');
+$crud = new CRUD;
+
+if (!isset($_SESSION['idUtilisateur'], $_POST['idPlante'], $_POST['idTypeEvenement'])) {
+    header('Location: evenement-create.php?erreur=donnees');
+    exit;
+}
+
+try {
+    $insert = $crud->insert('evenement', $_POST);
+
+    $_SESSION['planteSelectionnee'] = $_POST['idPlante'];
+    header("Location: ../../dreamplante.php");
+
+    exit;
+
+} catch (PDOException $e) {
+    header("Location: evenement-create.php");
+    exit;
+}
