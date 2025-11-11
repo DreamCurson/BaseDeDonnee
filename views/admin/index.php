6b18fb57868ae25544db23c4e8fa3fd825cdc61f
dreamcurson <dreamcurson@outlook.com>
2025-11-11 11:49:41 -0500

    Css du tableau finalisé BEM

--- views/admin/index.php
+++ views/admin/index.php
@@ -1,23 +1,30 @@
-{{ include('layouts/header.php', {title:'Dreamplante Admin'})}}
-    <h1>Liste de chaque plante</h1>
-    <table>
-        <tr>
-            <th>Nom</th>
-            <th>typePlante</th>
-            <th>dateAcquisition</th>
-            <th>utilisateur_idUtilisateur</th>
-        </tr>
-        {% for plante in plantes %}
-        <tr>
-            <td><a href="">{{ plante.nom }}</a></td>
-            <td>{{ plante.typePlante }}</td>
-            <td>{{ plante.dateAcquisition }}</td>
-            <td>{{ plante.utilisateur_idUtilisateur }}</td>
-        </tr>
-        {% endfor %}
-    </table>
+{{ include('layouts/headerAdmin.php', {title:'Dreamplante Admin'}) }}
 
-    <!-- clic sur plante donne evenement et note  -->
-{{ include('layouts/footer.php')}}
-</body>
-</html>
+<div class="table-plante-boite">
+    <h1 class="page-title">Liste des plantes</h1>
+
+    <table class="table-plante">
+        <thead class="table-plante__head">
+            <tr class="table-plante__row table-plante__row--header">
+                <th class="table-plante__cell table-plante__cell--head">Nom Plante</th>
+                <th class="table-plante__cell table-plante__cell--head">Type</th>
+                <th class="table-plante__cell table-plante__cell--head">Date d’acquisition</th>
+                <th class="table-plante__cell table-plante__cell--head">Utilisateur</th>
+            </tr>
+        </thead>
+        <tbody class="table-plante__body">
+            {% for plante in plantes %}
+            <tr class="table-plante__row">
+                <td class="table-plante__cell">
+                    <a href="" class="table-plante__link">{{ plante.nom }}</a>
+                </td>
+                <td class="table-plante__cell">{{ plante.typePlante }}</td>
+                <td class="table-plante__cell">{{ plante.dateAcquisition }}</td>
+                <td class="table-plante__cell">{{ plante.utilisateur_idUtilisateur }}</td>
+            </tr>
+            {% endfor %}
+        </tbody>
+    </table>
+</div>
+
+{{ include('layouts/footer.php') }}
