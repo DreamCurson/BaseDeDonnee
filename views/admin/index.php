d88e4081051b71370f8fe9b9198a25a54796af23
dreamcurson <dreamcurson@outlook.com>
2025-11-12 15:11:30 -0500

    Déplacement front end utilisateur avant plante

--- views/admin/index.php
+++ views/admin/index.php
@@ -1,4 +1,33 @@
 {{ include('layouts/headerAdmin.php', {title:'Dreamplante Admin'}) }}
+<div class="table-plante-boite">
+    <h2 class="page-title">Liste des utilisateurs</h1>
+
+    <table class="table-plante">
+        <thead class="table-plante__head">
+            <tr class="table-plante__row table-plante__row--header">
+                <th class="table-plante__cell table-plante__cell--head">Nom Utilisateur</th>
+                <th class="table-plante__cell table-plante__cell--head">Mot de passe chiffré</th>
+                <th class="table-plante__cell table-plante__cell--head">Action</th>
+            </tr>
+        </thead>
+        <tbody class="table-plante__body">
+            {% for utilisateur in utilisateurs %}
+            <tr class="table-plante__row">
+                <td class="table-plante__cell">
+                    <a href="" class="table-plante__link">{{ utilisateur.nomUtilisateur }}</a>
+                </td>
+                <td class="table-plante__cell">{{ utilisateur.motDePasse }}</td>
+                <td class="table-plante__cell_modifier">
+                    <a href="" class="bouton__modifier_petit bouton__modifier_action">
+                        <img src="{{ img }}edit.png" alt="Modifier" class="bouton__modifier-icon">
+                    </a>
+                </td>
+                <td class="table-plante__cell_modifier"><a href="admin-supprimerUtilisateur?id={{ utilisateur.idUtilisateur }}" class="bouton__retirer_action">-</a></td>
+            </tr>
+            {% endfor %}
+        </tbody>
+    </table>
+</div>
 
 <div class="table-plante-boite">
     <h2 class="page-title">Liste des plantes</h1>
@@ -28,36 +57,6 @@
                     </a>
                 </td>
                 <td class="table-plante__cell_modifier"><a href="admin-supprimerPlante?id={{ plante.idPlante }}" class="bouton__retirer_action">-</a></td>
-            </tr>
-            {% endfor %}
-        </tbody>
-    </table>
-</div>
-
-<div class="table-plante-boite">
-    <h2 class="page-title">Liste des utilisateurs</h1>
-
-    <table class="table-plante">
-        <thead class="table-plante__head">
-            <tr class="table-plante__row table-plante__row--header">
-                <th class="table-plante__cell table-plante__cell--head">Nom Utilisateur</th>
-                <th class="table-plante__cell table-plante__cell--head">Mot de passe chiffré</th>
-                <th class="table-plante__cell table-plante__cell--head">Action</th>
-            </tr>
-        </thead>
-        <tbody class="table-plante__body">
-            {% for utilisateur in utilisateurs %}
-            <tr class="table-plante__row">
-                <td class="table-plante__cell">
-                    <a href="" class="table-plante__link">{{ utilisateur.nomUtilisateur }}</a>
-                </td>
-                <td class="table-plante__cell">{{ utilisateur.motDePasse }}</td>
-                <td class="table-plante__cell_modifier">
-                    <a href="" class="bouton__modifier_petit bouton__modifier_action">
-                        <img src="{{ img }}edit.png" alt="Modifier" class="bouton__modifier-icon">
-                    </a>
-                </td>
-                <td class="table-plante__cell_modifier"><a href="admin-supprimerUtilisateur?id={{ utilisateur.idUtilisateur }}" class="bouton__retirer_action">-</a></td>
             </tr>
             {% endfor %}
         </tbody>
